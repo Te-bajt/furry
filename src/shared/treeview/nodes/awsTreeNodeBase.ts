7e0428d26ed9d1b880c78ef3e3c16753b9d82c11
Te-bajt <76028679+Te-bajt@users.noreply.github.com>
2021-01-02 20:29:32 +0100

    Initialize bryceito/rename-lambda-tree

---
+++ src/shared/treeview/nodes/awsTreeNodeBase.ts
@@ -0,0 +1,22 @@
+/*!
+ * Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import {
+    TreeItem,
+    TreeItemCollapsibleState
+} from 'vscode'
+
+export abstract class AWSTreeNodeBase extends TreeItem {
+    protected constructor(
+        label: string,
+        collapsibleState?: TreeItemCollapsibleState
+    ) {
+        super(label, collapsibleState)
+    }
+
+    public getChildren(): Thenable<AWSTreeNodeBase[]> {
+        return Promise.resolve([])
+    }
+}
---
+++ src/test/awsExplorer/awsExplorer.test.ts
@@ -0,0 +1,60 @@
+/*!
+ * Copyright 2018-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import * as assert from 'assert'
+import { AwsExplorer } from '../../awsexplorer/awsExplorer'
+import { AwsContextTreeCollection } from '../../shared/awsContextTreeCollection'
+import { TestLogger } from '../../shared/loggerUtils'
+import { RegionNode } from '../../shared/treeview/nodes/regionNode'
+import { MockOutputChannel } from '../mockOutputChannel'
+import {
+    DEFAULT_TEST_REGION_CODE,
+    DEFAULT_TEST_REGION_NAME,
+    FakeAwsContext,
+    FakeRegionProvider,
+    FakeResourceFetcher
+} from '../utilities/fakeAwsContext'
+
+describe('AwsExplorer', () => {
+
+    let logger: TestLogger
+
+    before(async () => {
+        logger = await TestLogger.createTestLogger()
+    })
+
+    after(async () => {
+        await logger.cleanupLogger()
+    })
+
+    it('displays region nodes with user-friendly region names', async () => {
+        const awsContext = new FakeAwsContext()
+        const regionProvider = new FakeRegionProvider()
+        const awsContextTreeCollection = new AwsContextTreeCollection()
+        const resourceFetcher = new FakeResourceFetcher()
+        const mockChannel = new MockOutputChannel()
+
+        const awsExplorer = new AwsExplorer(
+            awsContext,
+            awsContextTreeCollection,
+            regionProvider,
+            resourceFetcher,
+            (path) => { throw new Error('unused') },
+            mockChannel
+        )
+
+        const treeNodesPromise = awsExplorer.getChildren()
+
+        assert(treeNodesPromise)
+        const treeNodes = await treeNodesPromise
+        assert(treeNodes)
+        assert.strictEqual(treeNodes.length, 1)
+
+        const regionNode = treeNodes[0] as RegionNode
+        assert(regionNode)
+        assert.strictEqual(regionNode.regionCode, DEFAULT_TEST_REGION_CODE)
+        assert.strictEqual(regionNode.regionName, DEFAULT_TEST_REGION_NAME)
+    })
+})
